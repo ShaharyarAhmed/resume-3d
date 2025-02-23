75f228c448ba69189edf6544a0f2049753db09d6
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2025-02-23 10:02:08 +0000

    fix: javascript not imported

--- frontend/utils.js
+++ frontend/utils.js
@@ -5,18 +5,6 @@ const API_HOST = "http://localhost:8000"
 // const path = createPath(CENTER_VECTOR, new THREE.Vector3(800, 0, 1));
 // ground.add(path);
 
-export function createPath(start, end) {
-    const length = start.distanceTo(end); // Get distance between points
-    const road = new THREE.Mesh(
-        new THREE.PlaneGeometry(10, length), 
-        new THREE.MeshStandardMaterial({ color: 0xffffff, side: THREE.DoubleSide })
-    );
-    road.position.lerpVectors(start, end, 0.5); // Set position to the midpoint
-    road.receiveShadow = true;
-
-    return road;
-}
-
 /**
  *  Sumbits a resume to the API and returns a structured object.
  * @param {File} resumeFile - The file object containing the PDF resume.
