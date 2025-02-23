16aa8340e9943cfe1aeefac1f8ec6b091651e208
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2025-02-23 03:34:25 +0000

    I HATE CORSSS

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI, UploadFile
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import pymupdf
 
 from .gemini import GeminiClient
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://127.0.0.1:8080"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
 
 class Resume(BaseModel):
     file: UploadFile
